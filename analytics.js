e209ff9bf0644dbde51e106b6892cc69926724c3
naochan2026 <nh2808naokitm@gmail.com>
2026-07-03 23:30:12 +0900

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,1 +1,11 @@
+analytics.js
+// Google Analytics GA4
+const script = document.createElement("script");
+script.async = true;
+script.src = "https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX";
+document.head.appendChild(script);
 
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag("js", new Date());
+gtag("config", "G-XXXXXXXXXX");
